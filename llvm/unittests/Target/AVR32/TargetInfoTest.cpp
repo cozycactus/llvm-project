641ce58d9b06fd78ee3fc4ae082e28753a641671
Ruslan Migirov <cozycactus@mac.com>
2026-06-03 18:32:34 +0300

    Add AVR32 sscall instruction

--- llvm/unittests/Target/AVR32/TargetInfoTest.cpp
+++ llvm/unittests/Target/AVR32/TargetInfoTest.cpp
@@ -143,6 +143,7 @@ TEST(AVR32TargetInfo, LookupTarget) {
   EXPECT_EQ(MII->get(AVR32::RSUBrr).getSize(), 2u);
   EXPECT_EQ(MII->get(AVR32::SBCrrr).getSize(), 4u);
   EXPECT_EQ(MII->get(AVR32::SCALL).getSize(), 2u);
+  EXPECT_EQ(MII->get(AVR32::SSCALL).getSize(), 2u);
   EXPECT_EQ(MII->get(AVR32::SCRr).getSize(), 2u);
   EXPECT_EQ(MII->get(AVR32::SLEEPi).getSize(), 4u);
   EXPECT_EQ(MII->get(AVR32::SSRFi).getSize(), 2u);
@@ -1226,6 +1227,18 @@ TEST(AVR32TargetInfo, LookupTarget) {
   EXPECT_EQ(static_cast<uint8_t>(Code[0]), 0xd7);
   EXPECT_EQ(static_cast<uint8_t>(Code[1]), 0x33);
 
+  MCInst Sscall;
+  Sscall.setOpcode(AVR32::SSCALL);
+
+  Code.clear();
+  Fixups.clear();
+  MCE->encodeInstruction(Sscall, Code, Fixups, *STI);
+
+  EXPECT_TRUE(Fixups.empty());
+  ASSERT_EQ(Code.size(), 2u);
+  EXPECT_EQ(static_cast<uint8_t>(Code[0]), 0xd7);
+  EXPECT_EQ(static_cast<uint8_t>(Code[1]), 0x53);
+
   MCInst Scr;
   Scr.setOpcode(AVR32::SCRr);
   Scr.addOperand(MCOperand::createReg(AVR32::R1));
@@ -1867,6 +1880,13 @@ TEST(AVR32TargetInfo, LookupTarget) {
   EXPECT_EQ(Printed, "\tscall");
 
   Printed.clear();
+  raw_string_ostream SscallOS(Printed);
+  InstPrinter->printInst(&Sscall, /*Address=*/0, /*Annot=*/"", *STI,
+                         SscallOS);
+  SscallOS.flush();
+  EXPECT_EQ(Printed, "\tsscall");
+
+  Printed.clear();
   raw_string_ostream ScrOS(Printed);
   InstPrinter->printInst(&Scr, /*Address=*/0, /*Annot=*/"", *STI, ScrOS);
   ScrOS.flush();
@@ -1953,7 +1973,7 @@ TEST(AVR32TargetInfo, LookupTarget) {
   SourceMgr SrcMgr;
   SrcMgr.AddNewSourceBuffer(
       MemoryBuffer::getMemBuffer(
-          "nop\nfrs\nabs r1\nacr r1\nadc r1, r2, r3\naddabs r1, r2, r3\nadd r1, r2\nand r1, r2\nandn r1, r2\nasr r1, r2, r3\nbrev r1\nbreakpoint\ncasts.b r1\ncasts.h r1\ncastu.b r1\ncastu.h r1\nclz r1, r2\ncom r1\ncpc r1\ncpc r1, r2\ncp.w r1, r2\ncp.w r1, -1\ncp.w r1, 32\ncsrf 1\nneg r1\neor r1, r2\nicall r1\nlsl r1, r2, r3\nlsr r1, r2, r3\nmax r1, r2, r3\nmin r1, r2, r3\nmfdr r1, 4\nmfsr r1, 4\nmul r1, r2\nmul r1, r2, -1\nmul r1, r2, r3\nmuls.d r2, r3, r4\nmulu.d r2, r3, r4\nmusfr r1\nmustr r1\nmtdr 4, r1\nmtsr 4, r1\nor r1, r2\npopjc\npushjc\nretd\nrete\nret\nretal lr\nretcc lr\nretcs lr\nretlo lr\nretge lr\nretlt lr\nretmi lr\nretpl lr\nretls lr\nretgt lr\nretle lr\nrethi lr\nretvs lr\nretvc lr\nretqs lr\nreths lr\nreteq lr\nretne lr\nretj\nrets\nretss\nrol r1\nror r1\nrsub r1, r2\nsbc r1, r2, r3\nscall\nscr r1\nsleep 1\nssrf 1\nsub r1, r2\nswap.bh r1\nswap.b r1\nswap.h r1\nsync 1\ntlbr\ntlbs\ntlbw\ntst r1, r2\nmov r1, r2\nmov r1, -1\n"),
+          "nop\nfrs\nabs r1\nacr r1\nadc r1, r2, r3\naddabs r1, r2, r3\nadd r1, r2\nand r1, r2\nandn r1, r2\nasr r1, r2, r3\nbrev r1\nbreakpoint\ncasts.b r1\ncasts.h r1\ncastu.b r1\ncastu.h r1\nclz r1, r2\ncom r1\ncpc r1\ncpc r1, r2\ncp.w r1, r2\ncp.w r1, -1\ncp.w r1, 32\ncsrf 1\nneg r1\neor r1, r2\nicall r1\nlsl r1, r2, r3\nlsr r1, r2, r3\nmax r1, r2, r3\nmin r1, r2, r3\nmfdr r1, 4\nmfsr r1, 4\nmul r1, r2\nmul r1, r2, -1\nmul r1, r2, r3\nmuls.d r2, r3, r4\nmulu.d r2, r3, r4\nmusfr r1\nmustr r1\nmtdr 4, r1\nmtsr 4, r1\nor r1, r2\npopjc\npushjc\nretd\nrete\nret\nretal lr\nretcc lr\nretcs lr\nretlo lr\nretge lr\nretlt lr\nretmi lr\nretpl lr\nretls lr\nretgt lr\nretle lr\nrethi lr\nretvs lr\nretvc lr\nretqs lr\nreths lr\nreteq lr\nretne lr\nretj\nrets\nretss\nrol r1\nror r1\nrsub r1, r2\nsbc r1, r2, r3\nscall\nsscall\nscr r1\nsleep 1\nssrf 1\nsub r1, r2\nswap.bh r1\nswap.b r1\nswap.h r1\nsync 1\ntlbr\ntlbs\ntlbw\ntst r1, r2\nmov r1, r2\nmov r1, -1\n"),
       SMLoc());
 
   MCContext ParseCtx(TT, *MAI, *MRI, *STI, &SrcMgr);
