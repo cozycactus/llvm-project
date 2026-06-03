4b26ca21014b86400d13c166c90ae6fb08404f62
Ruslan Migirov <cozycactus@mac.com>
2026-06-03 10:25:51 +0300

    Add AVR32 rete instruction

--- llvm/unittests/Target/AVR32/TargetInfoTest.cpp
+++ llvm/unittests/Target/AVR32/TargetInfoTest.cpp
@@ -105,6 +105,7 @@ TEST(AVR32TargetInfo, LookupTarget) {
   EXPECT_EQ(MII->get(AVR32::MUSFRr).getSize(), 2u);
   EXPECT_EQ(MII->get(AVR32::NEGr).getSize(), 2u);
   EXPECT_EQ(MII->get(AVR32::ORrr).getSize(), 2u);
+  EXPECT_EQ(MII->get(AVR32::RETE).getSize(), 2u);
   EXPECT_EQ(MII->get(AVR32::RETJ).getSize(), 2u);
   EXPECT_EQ(MII->get(AVR32::ROLr).getSize(), 2u);
   EXPECT_EQ(MII->get(AVR32::RORr).getSize(), 2u);
@@ -657,6 +658,18 @@ TEST(AVR32TargetInfo, LookupTarget) {
   EXPECT_EQ(static_cast<uint8_t>(Code[0]), 0x04);
   EXPECT_EQ(static_cast<uint8_t>(Code[1]), 0x41);
 
+  MCInst Rete;
+  Rete.setOpcode(AVR32::RETE);
+
+  Code.clear();
+  Fixups.clear();
+  MCE->encodeInstruction(Rete, Code, Fixups, *STI);
+
+  EXPECT_TRUE(Fixups.empty());
+  ASSERT_EQ(Code.size(), 2u);
+  EXPECT_EQ(static_cast<uint8_t>(Code[0]), 0xd6);
+  EXPECT_EQ(static_cast<uint8_t>(Code[1]), 0x03);
+
   MCInst Retj;
   Retj.setOpcode(AVR32::RETJ);
 
@@ -1107,6 +1120,12 @@ TEST(AVR32TargetInfo, LookupTarget) {
   EXPECT_EQ(Printed, "\tor\tr1, r2");
 
   Printed.clear();
+  raw_string_ostream ReteOS(Printed);
+  InstPrinter->printInst(&Rete, /*Address=*/0, /*Annot=*/"", *STI, ReteOS);
+  ReteOS.flush();
+  EXPECT_EQ(Printed, "\trete");
+
+  Printed.clear();
   raw_string_ostream RetjOS(Printed);
   InstPrinter->printInst(&Retj, /*Address=*/0, /*Annot=*/"", *STI, RetjOS);
   RetjOS.flush();
@@ -1211,7 +1230,7 @@ TEST(AVR32TargetInfo, LookupTarget) {
   SourceMgr SrcMgr;
   SrcMgr.AddNewSourceBuffer(
       MemoryBuffer::getMemBuffer(
-          "nop\nfrs\nabs r1\nacr r1\nadc r1, r2, r3\naddabs r1, r2, r3\nadd r1, r2\nand r1, r2\nandn r1, r2\nasr r1, r2, r3\nbrev r1\ncasts.b r1\ncasts.h r1\ncastu.b r1\ncastu.h r1\ncom r1\ncpc r1\ncpc r1, r2\ncp.w r1, r2\ncp.w r1, -1\ncp.w r1, 32\ncsrf 1\nneg r1\neor r1, r2\nlsl r1, r2, r3\nlsr r1, r2, r3\nmax r1, r2, r3\nmin r1, r2, r3\nmul r1, r2\nmul r1, r2, -1\nmul r1, r2, r3\nmuls.d r2, r3, r4\nmulu.d r2, r3, r4\nmusfr r1\nor r1, r2\nretj\nrol r1\nror r1\nrsub r1, r2\nsbc r1, r2, r3\nscr r1\nssrf 1\nsub r1, r2\nswap.bh r1\nswap.b r1\nswap.h r1\ntlbr\ntlbs\ntlbw\ntst r1, r2\nmov r1, r2\nmov r1, -1\n"),
+          "nop\nfrs\nabs r1\nacr r1\nadc r1, r2, r3\naddabs r1, r2, r3\nadd r1, r2\nand r1, r2\nandn r1, r2\nasr r1, r2, r3\nbrev r1\ncasts.b r1\ncasts.h r1\ncastu.b r1\ncastu.h r1\ncom r1\ncpc r1\ncpc r1, r2\ncp.w r1, r2\ncp.w r1, -1\ncp.w r1, 32\ncsrf 1\nneg r1\neor r1, r2\nlsl r1, r2, r3\nlsr r1, r2, r3\nmax r1, r2, r3\nmin r1, r2, r3\nmul r1, r2\nmul r1, r2, -1\nmul r1, r2, r3\nmuls.d r2, r3, r4\nmulu.d r2, r3, r4\nmusfr r1\nor r1, r2\nrete\nretj\nrol r1\nror r1\nrsub r1, r2\nsbc r1, r2, r3\nscr r1\nssrf 1\nsub r1, r2\nswap.bh r1\nswap.b r1\nswap.h r1\ntlbr\ntlbs\ntlbw\ntst r1, r2\nmov r1, r2\nmov r1, -1\n"),
       SMLoc());
 
   MCContext ParseCtx(TT, *MAI, *MRI, *STI, &SrcMgr);
