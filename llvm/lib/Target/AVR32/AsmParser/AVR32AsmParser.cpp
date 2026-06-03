703d8b081b26a924a5d340e5947e0f9fb8d0a3c3
Ruslan Migirov <cozycactus@mac.com>
2026-06-03 17:04:54 +0300

    Add AVR32 retcc instruction

--- llvm/lib/Target/AVR32/AsmParser/AVR32AsmParser.cpp
+++ llvm/lib/Target/AVR32/AsmParser/AVR32AsmParser.cpp
@@ -287,7 +287,8 @@ bool AVR32AsmParser::parseInstruction(ParseInstructionInfo &Info,
              Name == "casts.b" || Name == "casts.h" || Name == "castu.h" ||
              Name == "castu.b" || Name == "com" || Name == "icall" ||
              Name == "musfr" ||
-             Name == "neg" || Name == "retal" || Name == "reteq" ||
+             Name == "neg" || Name == "retal" || Name == "retcc" ||
+             Name == "reteq" ||
              Name == "retne" ||
              Name == "rol" || Name == "ror" || Name == "scr" ||
              Name == "swap.b" || Name == "swap.bh" || Name == "swap.h") {
