bff02e290c1f9352a3121290e0545878ae405144
Ruslan Migirov <cozycactus@mac.com>
2026-06-03 17:58:37 +0300

    Add AVR32 retqs instruction

--- llvm/unittests/Target/AVR32/TargetInfoTest.cpp
+++ llvm/unittests/Target/AVR32/TargetInfoTest.cpp
@@ -125,6 +125,7 @@ TEST(AVR32TargetInfo, LookupTarget) {
   EXPECT_EQ(MII->get(AVR32::RETMIr).getSize(), 2u);
   EXPECT_EQ(MII->get(AVR32::RETNEr).getSize(), 2u);
   EXPECT_EQ(MII->get(AVR32::RETPLr).getSize(), 2u);
+  EXPECT_EQ(MII->get(AVR32::RETQSr).getSize(), 2u);
   EXPECT_EQ(MII->get(AVR32::RETVCr).getSize(), 2u);
   EXPECT_EQ(MII->get(AVR32::RETVSr).getSize(), 2u);
   EXPECT_EQ(MII->get(AVR32::RETJ).getSize(), 2u);
@@ -172,8 +173,8 @@ TEST(AVR32TargetInfo, LookupTarget) {
        {AVR32::RETALr, AVR32::RETCCr, AVR32::RETCSr, AVR32::RETEQr,
         AVR32::RETGEr, AVR32::RETGTr, AVR32::RETHIr, AVR32::RETHSr,
         AVR32::RETLEr, AVR32::RETLOr, AVR32::RETLSr, AVR32::RETLTr,
-        AVR32::RETMIr, AVR32::RETNEr, AVR32::RETPLr, AVR32::RETVCr,
-        AVR32::RETVSr})
+        AVR32::RETMIr, AVR32::RETNEr, AVR32::RETPLr, AVR32::RETQSr,
+        AVR32::RETVCr, AVR32::RETVSr})
     ExpectConditionalReturn(Opcode);
 
   EXPECT_EQ(MRI->getEncodingValue(AVR32::SP), 13u);
@@ -956,6 +957,19 @@ TEST(AVR32TargetInfo, LookupTarget) {
   EXPECT_EQ(static_cast<uint8_t>(Code[0]), 0x5e);
   EXPECT_EQ(static_cast<uint8_t>(Code[1]), 0xde);
 
+  MCInst Retqs;
+  Retqs.setOpcode(AVR32::RETQSr);
+  Retqs.addOperand(MCOperand::createReg(AVR32::LR));
+
+  Code.clear();
+  Fixups.clear();
+  MCE->encodeInstruction(Retqs, Code, Fixups, *STI);
+
+  EXPECT_TRUE(Fixups.empty());
+  ASSERT_EQ(Code.size(), 2u);
+  EXPECT_EQ(static_cast<uint8_t>(Code[0]), 0x5e);
+  EXPECT_EQ(static_cast<uint8_t>(Code[1]), 0xee);
+
   MCInst Reths;
   Reths.setOpcode(AVR32::RETHSr);
   Reths.addOperand(MCOperand::createReg(AVR32::LR));
@@ -1627,6 +1641,12 @@ TEST(AVR32TargetInfo, LookupTarget) {
   EXPECT_EQ(Printed, "\tretvc\tlr");
 
   Printed.clear();
+  raw_string_ostream RetqsOS(Printed);
+  InstPrinter->printInst(&Retqs, /*Address=*/0, /*Annot=*/"", *STI, RetqsOS);
+  RetqsOS.flush();
+  EXPECT_EQ(Printed, "\tretqs\tlr");
+
+  Printed.clear();
   raw_string_ostream RethsOS(Printed);
   InstPrinter->printInst(&Reths, /*Address=*/0, /*Annot=*/"", *STI, RethsOS);
   RethsOS.flush();
@@ -1779,7 +1799,7 @@ TEST(AVR32TargetInfo, LookupTarget) {
   SourceMgr SrcMgr;
   SrcMgr.AddNewSourceBuffer(
       MemoryBuffer::getMemBuffer(
-          "nop\nfrs\nabs r1\nacr r1\nadc r1, r2, r3\naddabs r1, r2, r3\nadd r1, r2\nand r1, r2\nandn r1, r2\nasr r1, r2, r3\nbrev r1\nbreakpoint\ncasts.b r1\ncasts.h r1\ncastu.b r1\ncastu.h r1\ncom r1\ncpc r1\ncpc r1, r2\ncp.w r1, r2\ncp.w r1, -1\ncp.w r1, 32\ncsrf 1\nneg r1\neor r1, r2\nicall r1\nlsl r1, r2, r3\nlsr r1, r2, r3\nmax r1, r2, r3\nmin r1, r2, r3\nmul r1, r2\nmul r1, r2, -1\nmul r1, r2, r3\nmuls.d r2, r3, r4\nmulu.d r2, r3, r4\nmusfr r1\nor r1, r2\npushjc\nretd\nrete\nretal lr\nretcc lr\nretcs lr\nretlo lr\nretge lr\nretlt lr\nretmi lr\nretpl lr\nretls lr\nretgt lr\nretle lr\nrethi lr\nretvs lr\nretvc lr\nreths lr\nreteq lr\nretne lr\nretj\nrets\nretss\nrol r1\nror r1\nrsub r1, r2\nsbc r1, r2, r3\nscall\nscr r1\nsleep 1\nssrf 1\nsub r1, r2\nswap.bh r1\nswap.b r1\nswap.h r1\nsync 1\ntlbr\ntlbs\ntlbw\ntst r1, r2\nmov r1, r2\nmov r1, -1\n"),
+          "nop\nfrs\nabs r1\nacr r1\nadc r1, r2, r3\naddabs r1, r2, r3\nadd r1, r2\nand r1, r2\nandn r1, r2\nasr r1, r2, r3\nbrev r1\nbreakpoint\ncasts.b r1\ncasts.h r1\ncastu.b r1\ncastu.h r1\ncom r1\ncpc r1\ncpc r1, r2\ncp.w r1, r2\ncp.w r1, -1\ncp.w r1, 32\ncsrf 1\nneg r1\neor r1, r2\nicall r1\nlsl r1, r2, r3\nlsr r1, r2, r3\nmax r1, r2, r3\nmin r1, r2, r3\nmul r1, r2\nmul r1, r2, -1\nmul r1, r2, r3\nmuls.d r2, r3, r4\nmulu.d r2, r3, r4\nmusfr r1\nor r1, r2\npushjc\nretd\nrete\nretal lr\nretcc lr\nretcs lr\nretlo lr\nretge lr\nretlt lr\nretmi lr\nretpl lr\nretls lr\nretgt lr\nretle lr\nrethi lr\nretvs lr\nretvc lr\nretqs lr\nreths lr\nreteq lr\nretne lr\nretj\nrets\nretss\nrol r1\nror r1\nrsub r1, r2\nsbc r1, r2, r3\nscall\nscr r1\nsleep 1\nssrf 1\nsub r1, r2\nswap.bh r1\nswap.b r1\nswap.h r1\nsync 1\ntlbr\ntlbs\ntlbw\ntst r1, r2\nmov r1, r2\nmov r1, -1\n"),
       SMLoc());
 
   MCContext ParseCtx(TT, *MAI, *MRI, *STI, &SrcMgr);
