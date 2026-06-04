a77249cfc8f5215470750472380d0baecc054bd3
Ruslan Migirov <cozycactus@mac.com>
2026-06-04 19:17:32 +0300

    Test AVR32 symbolic data relocations

---
+++ llvm/test/MC/AVR32/data-relocs.s
@@ -0,0 +1,18 @@
+# RUN: llvm-mc -triple=avr32 -filetype=obj %s -o %t.o
+# RUN: llvm-readobj -r -x .data %t.o | FileCheck %s
+
+.data
+.byte sym
+.short sym+1
+.long sym+2
+
+# CHECK: Relocations [
+# CHECK: Section {{.*}} .rela.data {
+# CHECK-NEXT: 0x0 R_AVR32_8 sym 0x0
+# CHECK-NEXT: 0x1 R_AVR32_16 sym 0x1
+# CHECK-NEXT: 0x3 R_AVR32_32 sym 0x2
+# CHECK-NEXT: }
+# CHECK-NEXT: ]
+
+# CHECK: Hex dump of section '.data':
+# CHECK-NEXT: 0x00000000 00000000 000000                     .......
