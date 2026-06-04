00c0ce9b78873ca441820fdac9b64d8c4590c434
Ruslan Migirov <cozycactus@mac.com>
2026-06-04 22:21:33 +0300

    Test AVR32 MC dynamic reloc directives

--- llvm/test/MC/AVR32/reloc-directive.s
+++ llvm/test/MC/AVR32/reloc-directive.s
@@ -6,11 +6,17 @@
 .reloc 0, R_AVR32_32, sym
 .reloc 2, R_AVR32_16, sym+4
 .reloc 0, R_AVR32_22H_PCREL, branch
+.reloc 0, R_AVR32_RELATIVE
+.reloc 0, R_AVR32_GLOB_DAT, ext
+.reloc 0, R_AVR32_JMP_SLOT, func
 
 # CHECK: Relocations [
 # CHECK: Section {{.*}} .rela.text {
 # CHECK-NEXT: 0x0 R_AVR32_32 sym 0x0
 # CHECK-NEXT: 0x2 R_AVR32_16 sym 0x4
 # CHECK-NEXT: 0x0 R_AVR32_22H_PCREL branch 0x0
+# CHECK-NEXT: 0x0 R_AVR32_RELATIVE - 0x0
+# CHECK-NEXT: 0x0 R_AVR32_GLOB_DAT ext 0x0
+# CHECK-NEXT: 0x0 R_AVR32_JMP_SLOT func 0x0
 # CHECK-NEXT: }
 # CHECK-NEXT: ]
