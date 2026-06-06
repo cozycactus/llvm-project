b9e53138541e5715f620aab9df23bec924c4ddca
Ruslan Migirov <cozycactus@mac.com>
2026-06-06 19:57:18 +0300

    [AVR32] Encode register moves in compact form

--- llvm/test/MC/AVR32/basic-instructions.s
+++ llvm/test/MC/AVR32/basic-instructions.s
@@ -11,6 +11,7 @@ scall
 add r0, r1
 sub r2, r3
 mov r4, r5
+moval r4, r5
 pushm r0-r12, lr
 popm r0-r12, lr
 
@@ -24,6 +25,7 @@ popm r0-r12, lr
 # CHECK-NEXT: scall # encoding: [0xd7,0x33]
 # CHECK-NEXT: add r0, r1 # encoding: [0x02,0x00]
 # CHECK-NEXT: sub r2, r3 # encoding: [0x06,0x12]
-# CHECK-NEXT: mov r4, r5 # encoding: [0xea,0x04,0x17,0xf0]
+# CHECK-NEXT: mov r4, r5 # encoding: [0x0a,0x94]
+# CHECK-NEXT: moval r4, r5 # encoding: [0xea,0x04,0x17,0xf0]
 # CHECK-NEXT: pushm r0-r3, r4-r7, r8-r9, r10, r11, r12, lr # encoding: [0xd7,0xf1]
 # CHECK-NEXT: popm r0-r3, r4-r7, r8-r9, r10, r11, r12, lr # encoding: [0xd7,0xf2]
